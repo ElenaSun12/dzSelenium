392bbd62d18efb3b94b33da2297956a4d05af23e
Netology Test <elen.1996@list.ru>
2025-03-17 15:39:38 +0300

    Added code happy path

--- src/test/java/ru/netology/ordercard/OrderCardTest.java
+++ src/test/java/ru/netology/ordercard/OrderCardTest.java
@@ -7,6 +7,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class OrderCardTest {
     private WebDriver driver;
@@ -21,7 +25,7 @@ public class OrderCardTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
+        //options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("http://localhost:9999");
@@ -34,3 +38,19 @@ public class OrderCardTest {
     }
 
     @Test
+    public void shouldSendCorrectData() {
+//        List<WebElement> inputElements = driver.findElements(By.cssSelector("input")); вариант кода для открытия страницы и заполнения полей Имя и Номер
+//        inputElements.get(0).sendKeys("Василий");
+//        inputElements.get(1).sendKeys("+79001234567");
+
+        WebElement formElement = driver.findElement(By.cssSelector("form"));
+        formElement.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Васильев Василий");
+        formElement.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79001234567");
+
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        WebElement actual = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
+        assertTrue(actual.isDisplayed());
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actual.getText().trim());
+    }
+}
